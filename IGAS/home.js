1f9c40d1ee64fe14f403dc503d3f3364581da0bf
igas3 <iasilva1343@gmail.com>
2023-08-02 21:01:41 -0300

    New

---
+++ IGAS/home.js
@@ -0,0 +1,40 @@
+const nome = document.getElementById("nomeh")
+const senha = document.getElementById("senhah")
+const loginbtn = document.getElementById("loginbtn")
+
+
+const login = {
+    user: "italo",
+    senha: "2fast4u"
+}
+
+loginbtn.addEventListener("click", () => {
+    const nomeValor = nome.value
+    const senhaValor = senha.value
+
+    if (nomeValor !== "" && senhaValor !== "") {
+        console.log("okay");
+        if (login.user === nomeValor
+
+        ) {
+            if (login.senha === senhaValor) {
+
+                window.location.href = "login.html"
+            }
+            else {
+                alert("Senha errada")
+            }
+        }
+        else {
+            alert("Senha ou Login Errado")
+        }
+
+    } else {
+        alert("seu login não pode estar vazio")
+    }
+
+
+})
+
+
+
